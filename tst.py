5a74f5f2a3879f7d218b55a9a1044e59e1b0b461
Akinloluwa <akinloluwa12345@gmail.com>
2023-12-17 19:45:24 +0100

    first commit

---
+++ tst.py
@@ -0,0 +1,16 @@
+# Number1:
+# user_name = str(input("Enter your name: "))
+# print("The length of your name is = {}".format( len(user_name)))
+
+# Number3:
+# n = int(input("Enter the value of n: "))
+
+# for i in range(n, 0, -1):
+#     print("Hello World", i, "times remaining.")
+    
+# Number4:
+# n = int(input("Enter the value of n: "))
+
+# while n > 0 :
+#     print("Hello World", n, "times remaining.")
+#     n -= 1 
